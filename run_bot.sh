c58b775054a819ec1be44f220cf06bb8d27f1acf
domomb <domdbomb13@gmail.com>
2024-09-28 21:10:46 -0400

    Update run_bot.sh
    Updated to xPrinny's fork

--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the default version to latest
-VER_DEFAULT=$(curl --silent "https://api.github.com/repos/jagrosh/MusicBot/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+VER_DEFAULT=$(curl --silent "https://api.github.com/repos/xPrinny/MusicBot/releases" | grep -Po '"tag_name": "\K.*?(?=")')
 
 # If the ENV is not explicitly set, use the sourced ENV from the Dockerfile
 if [ $BOT_VERSION == "latest" ]; then
@@ -10,7 +10,7 @@ fi
 
 echo -e "Downloading JMusicBot $BOT_VERSION"
 if [ ! -f JMusicBot-$BOT_VERSION.jar ]; then	
-  wget https://github.com/jagrosh/MusicBot/releases/download/$BOT_VERSION/JMusicBot-$BOT_VERSION.jar
+  wget https://github.com/xPrinny/MusicBot/releases/download/$BOT_VERSION/JMusicBot-$BOT_VERSION.jar
 fi
 
 echo -e "Starting JMusicBot $BOT_VERSION"
